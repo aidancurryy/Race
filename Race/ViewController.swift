7053353e0584b7d32d81e67a96c629bade590ab0
aidancurryy <aidancurryy@gmail.com>
2020-10-26 09:18:13 -0400

    Race

--- Race/ViewController.swift
+++ Race/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var car1 = Car()
+        
+        var car2 = Car(theName: "B", theLane: 2, theSpeed: 1.1)
+        
+        var car3 = Car(theName: "C", theLane: 3, theSpeed: 1.1)
+        
+        var car4 = Car(theName: "D", theLane: 4, theSpeed: 1.1)
+        
+        
     }
 
 
---
+++ Race/car.swift
@@ -0,0 +1,36 @@
+//
+//  car.swift
+//  Race
+//
+//  Created by  on 10/26/20.
+//  Copyright © 2020 ChemsApps. All rights reserved.
+//
+
+import Foundation
+
+class Car
+{
+    var name: String
+    var lane: Int
+    var speed: Double
+    
+    init()
+    {
+        name = "E"
+        lane = 1
+        speed = 3.2
+        getRandomSpeed()
+    }
+    init(theName: String, theLane: Int, theSpeed: Double)
+    {
+        name = theName
+        lane = theLane
+        speed = theSpeed
+        getRandomSpeed()
+    }
+    func getRandomSpeed()
+    {
+        speed = Double.random(in: 2...5)
+    }
+}
+
